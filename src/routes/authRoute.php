f021466e2a90b61b6c90ffb675d1ea3d14c769b3
mnuel1 <mnuelmrin21@gmail.com>
2024-04-02 00:59:14 +0800

    update

--- src/routes/authRoute.php
+++ src/routes/authRoute.php
@@ -3,11 +3,12 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
     require_once $_SERVER['DOCUMENT_ROOT'] . '/src/controller/auth.php';
   
-    // if ($_SERVER['REQUEST_METHOD'] === 'POST' 
-    //     && $_POST['route'] === '/login') {
+    // for login
+    // route: /login
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' 
+        && $_POST['route'] === 'login') {
         include '../middleware/apiRateLimiter.php';
-        $_POST['username'] = 'test';
-        $_POST['password'] = 'test';
+  
         // Check if username and password are set
         if (isset($_POST['username']) && isset($_POST['password'])) {
             $username = $_POST['username'];
@@ -15,13 +16,12 @@
                 
             // Call login function
             $response = login($connect, $username, $password);
-            echo $_SESSION['accessLevel'];
-            echo json_encode($response  );
+            echo json_encode($response);
         } else {
             // Output error message
             echo "Username and/or password are empty";
         }
-    // }
+    }
 
 
 ?>
